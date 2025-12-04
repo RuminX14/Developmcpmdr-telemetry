b0119a899fd667ac9dc718bf664571c774c38b4e
RuminX14 <marcinrumins@gmail.com>
2025-12-04 13:13:56 +0100

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1826,13 +1826,15 @@
               borderDash: [4, 3]
             },
             {
-              label: 'Kierunek [°] (wznoszenie)',
-              xAxisID: 'xDir',
-              yAxisID: 'y',
-              data: [],
-              showLine: false,
-              pointRadius: 2,
-              borderWidth: 1.2
+            label: 'Kierunek [°] (wznoszenie)',
+            xAxisID: 'xDir',
+            yAxisID: 'y',
+            data: [],
+            showLine: false,
+            pointRadius: 3,
+            borderWidth: 1.2,
+            borderColor: '#b38cff',      // fioletowe obramowanie punktów
+            backgroundColor: '#b38cff'   // fioletowe wypełnienie punktów i prostokąta w legendzie
             },
             {
               label: 'Kierunek [°] (opadanie)',
